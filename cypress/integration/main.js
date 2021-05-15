54f84b9dace9358d9a0234ef446f8092c0c26f74
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2021-05-15 13:27:43 +0100

    ✅ Add a test for channel loading

--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -1,5 +1,13 @@
+/// <reference types="Cypress" />
+
 describe("Navigate to Discord Explorer", () => {
     it("Opens up the local Snowpack Dev Server", () => {
         cy.visit("http://localhost:8080")
+    })
+})
+describe("Channel loading works", () => {
+    it("Clicks on a sidebar item and checks that messages appear", () => {
+        cy.get(".sidebar").contains("mind-is-tree").click()
+        cy.get(".message-card")
     })
 })
